b1b6d014a0ef1fe6cd124e166a27efafa80ef1b1
Daniele Di Eleuterio <daniele.dieleuterio@gmail.com>
2022-02-15 15:17:03 +0100

    correzioni e aggiornamento create

--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -6,45 +6,57 @@
         @csrf
         <div class="form-group">
             <label for="title">Title:</label>
-            <input type="text" class="form-control" id="title" name="title" placeholder="Insert title">
+            <input type="text" class="form-control @error('title') is-invalid @enderror" id="title" name="title" placeholder="Insert title" value="{{old("title")}}">
+            @error('title')
+                <div class="alert alert-danger">{{ $message }}</div>
+            @enderror
         </div>
         <div class="form-group">
-            <label for="description">Description:</label>
-            <textarea class="form-control" id="description" name="description" rows="6" placeholder="Insert description"></textarea>
+            <label for="description">Description</label>
+            <textarea class="form-control @error('description') is-invalid @enderror" id="description" name="description" rows="6" placeholder="Insert description">{{old("description")}}</textarea>
+            @error('description')
+                <div class="alert alert-danger">{{ $message }}</div>
+            @enderror
         </div>
         <div class="form-group">
-            <label for="thumb">Thumb:</label>
-            <input type="text" class="form-control" id="thumb" name="thumb" placeholder="Insert thumb">
+            <label for="thumb">thumb:</label>
+            <input type="text" class="form-control @error('thumb') is-invalid @enderror" id="thumb" name="thumb" placeholder="Insert thumb" value="{{old("thumb")}}">
+            @error('thumb')
+                <div class="alert alert-danger">{{ $message }}</div>
+            @enderror
         </div>
         <div class="form-group">
-            <label for="price">Price:</label>
-            <input type="number" step="0.01" class="form-control" id="price" name="price" placeholder="Insert price">
+            <label for="price">price:</label>
+            <input type="number" step="0.01" class="form-control  @error('price') is-invalid @enderror" id="price" name="price" placeholder="Insert price" value="{{old("price")}}">
+            @error('price')
+                <div class="alert alert-danger">{{ $message }}</div>
+            @enderror
         </div>
         <div class="form-group">
             <label for="series">Series:</label>
-            <input type="text" class="form-control" id="series" name="series" placeholder="Insert series">
+            <input type="text" class="form-control @error('series') is-invalid @enderror" id="series" name="series" placeholder="Insert series" value="{{old("series")}}">
+            @error('series')
+                <div class="alert alert-danger">{{ $message }}</div>
+            @enderror
         </div>
         <div class="form-group">
             <label for="sale_date">Sale date:</label>
-            <input type="text" class="form-control" id="sale_date" name="sale_date" placeholder="Insert date, format AAAA/MM/GG">
+            <input type="text" class="form-control @error('sale_date') is-invalid @enderror" id="sale_date" name="sale_date" placeholder="Insert date, format AAAA/MM/GG" value="{{old("sale_date")}}">
+            @error('sale_date')
+                <div class="alert alert-danger">{{ $message }}</div>
+            @enderror
         </div>
         <div class="form-group">
             <label for="type">Type:</label>
-            <select class="custom-select form-control text-secondary" id="type" name="type">
-                <option value="comic book">comic book</option>
-                <option value="graphic novel">graphic novel</option>
+            <select class="custom-select form-control text-secondary @error('type') is-invalid @enderror" id="type" name="type">
+                <option value="comic book" {{old("type") == "comic book" ? "selected" : null}}>comic book</option>
+                <option value="graphic novel" {{old("type") == "graphic novel" ? "selected" : null}}>graphic novel</option>
             </select>
+            @error('type')
+                <div class="alert alert-danger">{{ $message }}</div>
+            @enderror
         </div>
+
         <button type="submit" class="btn btn-primary">Submit</button>
     </form>
-
-    @if ($errors->any())
-    <div class="alert alert-danger">
-        <ul>
-            @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
-            @endforeach
-        </ul>
-    </div>
-    @endif
 @endsection 
